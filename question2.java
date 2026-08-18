23430da3ce1becf2a01598a5a0fdf82a61b36a88
debolina2006 <Chowdhurydebolina04@gmail.com>
2026-08-18 15:33:13 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println("Sum = " + (a + b));
+
+    }
+}
